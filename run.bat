ca136aaa53cbe7d8261a1ed7d4800e6e5a965f1a
maelli-zip <m.paarmann07@gmail.com>
2026-02-17 20:40:06 +0100

    fix: update run command to allow external access

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 echo Starte Frontend und Backend...
 
 :: Frontend (Angular)
-start cmd /k "cd frontend && ng serve"
+start cmd /k "cd frontend && ng serve --host 0.0.0.0"
 
 :: Backend (Gradle)
 start cmd /k "cd backend && gradlew bootRun"
