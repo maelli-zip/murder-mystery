02fe2aad4aae6f65dc6776c9eb3e5de3eb67ecae
maelli-zip <m.paarmann07@gmail.com>
2026-02-17 21:56:46 +0100

    feat: add dashboard component with navigation and card layout

---
+++ frontend/src/app/shared/card/card.ts
@@ -0,0 +1,12 @@
+import { Component, Input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.scss',
+})
+export class Card {
+  @Input() content: string = '';
+}
